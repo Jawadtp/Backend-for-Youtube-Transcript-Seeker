5098a00aff347dda94010a6b29f8265b410a7056
batman <batman@gnomebat.com>
2021-06-13 19:09:55 +0530

    cleaned up some stuff

--- server.py
+++ server.py
@@ -5,24 +5,15 @@ app = Flask(__name__)
 
 print('Server is up and running!')
 
-@app.route('/')
-def student():
-   return render_template('ui.html')
-
-
 @app.route('/',methods = ['POST', 'GET'])
 def result():
+   if request.method == 'GET':
+      return render_template('ui.html')
    if request.method == 'POST':
       result = request.form
-      print(result['url'])
-      print(result['word'])
-      temp=[]
+      temp={}
       temp = yoogle.main(result['url'], result['word'])
-      print(temp)
       return render_template("ui.html", temp=temp, url=result['url'], word=result['word'])
 
-
-if __name__ == 'main':
+if __name__=="__main__":
    app.run(debug = False)
-
-   
